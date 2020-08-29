ac69c83d42e892fe5159a7ebc51cb3bb76f3761e
Vinícius Bohrer <vbsanttos@gmail.com>
2020-08-29 19:10:04 -0300

    basic todo-list with vue and axios

---
+++ src/components/TodoItem.vue
@@ -0,0 +1,53 @@
+<template>
+  <div class="todo-item" v-bind:class="{ 'is-complete': todo.completed }">
+    <p>
+      <input v-model="checked" type="checkbox" @click="toggleTodo" />
+      {{ todo.title }}
+      <!-- <small>
+        <i>{{JSON.stringify(todo)}}</i>
+      </small>-->
+      <!-- <button @click="$emit('del-todo', todo.id)" class="del">X</button> -->
+      <button @click="deleteTodo" class="del">X</button>
+    </p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "TodoItem",
+  props: ["todo"],
+  data: function() {
+    return {
+      checked: this.todo.completed,
+    };
+  },
+  methods: {
+    toggleTodo() {
+      this.$emit("toggle-todo", this.todo);
+    },
+    deleteTodo() {
+      this.$emit("del-todo", this.todo.id);
+    },
+  },
+};
+</script>
+
+<style scoped>
+.todo-item {
+  background: #f4f4f4;
+  padding: 10px;
+  border-bottom: 1px #ccc dotted;
+}
+.is-complete {
+  text-decoration: line-through;
+}
+.del {
+  background: #ff0000;
+  color: #fff;
+  border: none;
+  padding: 5px 9px;
+  border-radius: 50%;
+  cursor: pointer;
+  float: right;
+}
+</style>
---
+++ src/components/Todos.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    <div v-bind:key="todo.id" v-for="todo in todos">
+      <TodoItem
+        v-bind:todo="todo"
+        v-on:del-todo="$emit('del-todo', todo.id)"
+        v-on:toggle-todo="$emit('toggle-todo', todo)"
+      />
+    </div>
+  </div>
+</template>
+
+<script>
+// Import Components
+import TodoItem from "./TodoItem";
+export default {
+  name: "Todos",
+  components: {
+    TodoItem,
+  },
+  props: ["todos"],
+};
+</script>
