957da0566a08dc769c82b64c7a94b23aaa4f50b6
Vinícius Bohrer <vbsanttos@gmail.com>
2020-09-03 18:46:20 -0300

    remove console logs

--- src/store/index.js
+++ src/store/index.js
@@ -9,46 +9,36 @@ export default createStore({
   },
   mutations: {
     SET_LOADING_STATUS(state, status) {
-      console.log(mutationTypes.SET_LOADING_STATUS, status);
       state.loading = status;
     },
     SET_TODOS(state, todos) {
-      console.log(mutationTypes.SET_TODOS, todos);
       state.todos = todos;
     },
   },
   actions: {
     async loadTodos({ commit }) {
-      console.groupCollapsed("loadTodos");
       commit(mutationTypes.SET_LOADING_STATUS, true);
-      const allTodos = await Storage.loadTodos();
+      const todos = await Storage.loadTodos();
       commit(mutationTypes.SET_LOADING_STATUS, false);
-      commit(mutationTypes.SET_TODOS, allTodos);
-      console.groupEnd("loadTodos");
+      commit(mutationTypes.SET_TODOS, todos);
     },
     async addTodo({ commit }, todo) {
-      console.groupCollapsed("addTodo");
       commit(mutationTypes.SET_LOADING_STATUS, true);
-      const allTodos = await Storage.addTodo(todo);
+      const todos = await Storage.addTodo(todo);
       commit(mutationTypes.SET_LOADING_STATUS, false);
-      commit(mutationTypes.SET_TODOS, allTodos);
-      console.groupEnd("addTodo");
+      commit(mutationTypes.SET_TODOS, todos);
     },
     async toggleTodo({ commit }, todo) {
-      console.groupCollapsed("toggleTodo");
       commit(mutationTypes.SET_LOADING_STATUS, true);
-      const allTodos = await Storage.toggleTodo(todo);
+      const todos = await Storage.toggleTodo(todo);
       commit(mutationTypes.SET_LOADING_STATUS, false);
-      commit(mutationTypes.SET_TODOS, allTodos);
-      console.groupEnd("toggleTodo");
+      commit(mutationTypes.SET_TODOS, todos);
     },
     async deleteTodo({ commit }, todo_id) {
-      console.groupCollapsed("deleteTodo");
       commit(mutationTypes.SET_LOADING_STATUS, true);
-      const allTodos = await Storage.deleteTodo(todo_id);
+      const todos = await Storage.deleteTodo(todo_id);
       commit(mutationTypes.SET_LOADING_STATUS, false);
-      commit(mutationTypes.SET_TODOS, allTodos);
-      console.groupEnd("deleteTodo");
+      commit(mutationTypes.SET_TODOS, todos);
     },
   },
   modules: {},
